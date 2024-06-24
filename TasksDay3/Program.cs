3cae50833634453f5098be24e65a6a92c60ebd10
imelbek <irakhimov98@gmail.com>
2024-06-24 21:19:56 +0500

    Could do only the 1st one(

--- TasksDay3/Program.cs
+++ TasksDay3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task #2 from Paper 3
+// >500 -> 3%, >1000 -> 5%
+
+Console.WriteLine("Give an item a price: ");
+double itemPrice = double.Parse(Console.ReadLine());
+
+double totalPrice = 1;
+
+if(itemPrice > 500 && itemPrice <= 1000) totalPrice = itemPrice * 0.97;
+else if(itemPrice > 1000) totalPrice = itemPrice * 0.95;
+else totalPrice = itemPrice;
+
+Console.WriteLine(totalPrice);
+Console.WriteLine("End of Task #2 \n");
+
